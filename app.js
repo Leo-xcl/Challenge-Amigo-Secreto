8bab7049e22dff8baaf5ea8a086824c53f02b0ec
Leopoldo <leopoldojavih@gmail.com>
2025-08-09 16:26:41 -0600

    Agrega el valor ingresado

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigosSorteados = [];
+
+let amigoIngresado = document.getElementById("amigo").value;
+
+function limpiarcaja(){
+    document.getElementById("amigo").value = "";
+}
+
+function validarLaEntrada(){
+    if (amigoIngresado = ""){
+        alert("Por favor ingrese un nombre");
+    } else{
+        amigosSorteados.push(amigoIngresado);
+        limpiarcaja();
+    }
+}
